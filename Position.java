7fca05540a30326535628a87e5af0a1d0a6314a4
Jack <porterjp1@appstate.edu>
2024-02-13 18:10:39 -0500

    Add invalid default position

--- Position.java
+++ Position.java
@@ -1,20 +1,21 @@
 public enum Position 
 {
-    TOP_LEFT(0,0),
-    TOP_CENTER(0,1),
-    TOP_RIGHT(0, 2),
-    MIDDLE_LEFT(1, 0),
-    CENTER(1, 1),
-    MIDDLE_RIGHT(1, 2),
-    BOTTOM_LEFT(2,0),
-    BOTTOM_CENTER(2, 1),
-    BOTTOM_RIGHT(2, 2);
+    TOP_LEFT(0,0, false),
+    TOP_CENTER(0,1, false),
+    TOP_RIGHT(0, 2, false),
+    MIDDLE_LEFT(1, 0, false),
+    CENTER(1, 1, false),
+    MIDDLE_RIGHT(1, 2, false),
+    BOTTOM_LEFT(2,0, false),
+    BOTTOM_CENTER(2, 1, false),
+    BOTTOM_RIGHT(2, 2, false),
+    INVALID_POSITION(-1, -1, true);
 
     private int row;
     private int column;
     private boolean taken;
 
-    Position(int row, int column)
+    Position(int row, int column, boolean taken)
     {
         this.row = row;
         this.column = column;
@@ -37,6 +38,10 @@ public enum Position
 
     public void setTaken(boolean taken)
     {
-        this.taken = taken;
+        if (this != INVALID_POSITION)
+        {
+            this.taken = taken;
+        }
+
     }
 }
