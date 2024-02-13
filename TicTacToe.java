7944210a808d5cc52f7112e4bc81d76500b859ea
Jack <porterjp1@appstate.edu>
2024-02-13 14:47:04 -0500

    Add scanner field for capturing player move

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TicTacToe
 {
     public static final int ROWS = 3, COLUMNS = 3;
@@ -5,6 +7,8 @@ public class TicTacToe
     public static int[][] moveTracker = new int[ROWS][COLUMNS];
 
     public static char[][] gameBoard = new char[ROWS][COLUMNS];
+
+    public Scanner moveCollector;
     
     
     public void printGameBoard()
