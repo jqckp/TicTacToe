9d7626103220dbc4ae7027edc63046a0b99a377c
Jack <porterjp1@appstate.edu>
2024-02-13 15:34:47 -0500

    Add positions for constants

--- Position.java
+++ Position.java
@@ -1,15 +1,21 @@
 public enum Position 
 {
-    TOP_LEFT,
-    TOP_CENTER,
-    TOP_RIGHT,
-    MIDDLE_LEFT,
-    CENTER,
-    MIDDLE_RIGHT,
-    BOTTOM_LEFT,
-    BOTTOM_CENTER,
-    BOTTOM_RIGHT;
+    TOP_LEFT(0,0),
+    TOP_CENTER(0,1),
+    TOP_RIGHT(0, 2),
+    MIDDLE_LEFT(1, 0),
+    CENTER(1, 1),
+    MIDDLE_RIGHT(1, 2),
+    BOTTOM_LEFT(2,0),
+    BOTTOM_CENTER(2, 1),
+    BOTTOM_RIGHT(2, 2);
 
     private int row;
     private int column;
+
+    Position(int row, int column)
+    {
+        this.row = row;
+        this.column = column;
+    }
 }
