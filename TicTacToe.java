8013df5d333fc2086cd81301952c30bc40c5f317
Jack <porterjp1@appstate.edu>
2024-02-12 19:50:39 -0500

    Add array to track player moves

---
+++ TicTacToe.java
@@ -0,0 +1,18 @@
+public class TicTacToe
+{
+    public static final int ROWS = 3, COLUMNS = 3;
+
+    public static int[][] moveTracker = new int[ROWS][COLUMNS];
+
+
+
+    public static void main(String[] args) 
+    {
+        
+    }
+
+    
+
+
+
+}
