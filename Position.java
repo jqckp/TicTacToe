888810adafee20190d514711935cc6856079e8ed
Jack <porterjp1@appstate.edu>
2024-02-13 15:30:54 -0500

    Add constants and associated fields

--- Position.java
+++ Position.java
@@ -1,4 +1,15 @@
 public enum Position 
 {
-    
+    TOP_LEFT,
+    TOP_CENTER,
+    TOP_RIGHT,
+    MIDDLE_LEFT,
+    CENTER,
+    MIDDLE_RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_CENTER,
+    BOTTOM_RIGHT;
+
+    private int row;
+    private int column;
 }
