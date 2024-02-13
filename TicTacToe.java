6ed50d5ae2a31c97c6bff7c07381c7e868bdbf49
Jack <porterjp1@appstate.edu>
2024-02-13 14:41:47 -0500

    Add game board

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,11 +4,16 @@ public class TicTacToe
 
     public static int[][] moveTracker = new int[ROWS][COLUMNS];
 
+    public char[][] gameBoard = new char[ROWS][COLUMNS];
+    
+       
 
+       
 
     public static void main(String[] args) 
     {
         
+       
     }
 
     
