9c6b560ee4362b6c8ef846af9ccb669de1ba0be7
Jack <porterjp1@appstate.edu>
2024-02-14 16:50:16 -0500

    Define fields and constructor

--- WinCondtion.java
+++ WinCondtion.java
@@ -1,13 +1,24 @@
 public enum WinCondtion 
 {
-    TOP_HORIZONTAL,
-    MIDDLE_HORIZONTAL,
-    BOTTOM_HORIZONTAL,
-    LEFT_VERTICAL,
-    MIDDLE_VERTICAL,
-    RIGHT_VERTICAL,
-    TOP_LEFT_TO_BOTTOM_RIGHT,
-    TOP_RIGHT_TO_BOTTOM_LEFT;
+    TOP_HORIZONTAL(Position.TOP_LEFT, Position.TOP_CENTER, Position.TOP_RIGHT),
+    MIDDLE_HORIZONTAL(Position.MIDDLE_LEFT, Position.CENTER, Position.MIDDLE_RIGHT),
+    BOTTOM_HORIZONTAL(Position.BOTTOM_LEFT, Position.BOTTOM_CENTER, Position.BOTTOM_RIGHT),
+    LEFT_VERTICAL(Position.TOP_LEFT, Position.MIDDLE_LEFT, Position.MIDDLE_RIGHT),
+    MIDDLE_VERTICAL(Position.TOP_CENTER, Position.CENTER, Position.BOTTOM_CENTER),
+    RIGHT_VERTICAL(Position.TOP_RIGHT, Position.MIDDLE_RIGHT, Position.BOTTOM_RIGHT),
+    TOP_LEFT_TO_BOTTOM_RIGHT(Position.TOP_LEFT, Position.CENTER, Position.BOTTOM_RIGHT),
+    TOP_RIGHT_TO_BOTTOM_LEFT(Position.TOP_RIGHT, Position.TOP_CENTER, Position.BOTTOM_LEFT);
+
+    Position posOne;
+    Position posTwo;
+    Position posThree;
+
+    WinCondtion(Position posOne, Position posTwo, Position posThree)
+    {
+        this.posOne = posOne;
+        this.posTwo = posTwo;
+        this.posThree = posThree;
+    }
 
     
 }
