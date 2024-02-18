dcd33c999516d19fafb23b9c5b5dde5baa1bf45b
Jack <porterjp1@appstate.edu>
2024-02-17 19:18:07 -0500

    Modify board printing

--- TicTacToe.java
+++ TicTacToe.java
@@ -90,10 +90,12 @@ public class TicTacToe
     
     private void printGameBoard()
     {
-        System.out.println(gameBoard[0][0] + " " + gameBoard[0][1] + " " + gameBoard[0][2]);
-        System.out.println(gameBoard[1][0] + " " + gameBoard[1][1] + " " + gameBoard[1][2]);
-        System.out.println(gameBoard[2][0] + " " + gameBoard[2][1] + " " + gameBoard[2][2]);
+        System.out.println(gameBoard[0][0] + " | " + gameBoard[0][1] + " | " + gameBoard[0][2]);
+        System.out.println("-----------");
+        System.out.println(gameBoard[1][0] + " | " + gameBoard[1][1] + " | " + gameBoard[1][2]);
         System.out.println("--------------");
+        System.out.println(gameBoard[2][0] + " | " + gameBoard[2][1] + " | " + gameBoard[2][2]);
+        System.out.println();
     }
 
     /**
