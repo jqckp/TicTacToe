fa2cbd94c248cd32a4e1e1ffdf1e4d9d84211383
Jack <porterjp1@appstate.edu>
2024-02-18 10:56:04 -0500

    Adjust board design

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -117,9 +117,9 @@ public class TicTacToe
     private void printGameBoard()
     {
         System.out.println(gameBoard[0][0] + " | " + gameBoard[0][1] + " | " + gameBoard[0][2]);
-        System.out.println("-----------");
+        System.out.println("----------");
         System.out.println(gameBoard[1][0] + " | " + gameBoard[1][1] + " | " + gameBoard[1][2]);
-        System.out.println("-----------");
+        System.out.println("----------");
         System.out.println(gameBoard[2][0] + " | " + gameBoard[2][1] + " | " + gameBoard[2][2]);
         System.out.println();
     }
